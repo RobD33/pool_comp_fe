271db094c60f2c0393b8c0b1988c018a59ee1ea6
Rob Davidson <rob@robs-mbp.mynet>
2020-02-16 20:29:03 +0000

    validate user signup

---
+++ src/components/signup/Signup.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import { getUsers, createUser } from '../../utils/connection'
+
+class Login extends React.Component {
+
+    state = {
+      name:'',
+      password: '',
+      confirmPassword: '',
+      email: '',
+      matchPassword: true,
+      users:[],
+      loadedUsers: true,
+      uniqueName: true
+    }
+  
+  render() {
+    return (
+      <div>
+        <div>
+          <label>Username:</label>
+          <input type="text" value={this.state.name} onChange={this.handleNameChange} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={this.state.password} onChange={this.handlePasswordChange} />
+        </div>
+        <div>
+          <label>Confirm password:</label>
+          <input type="password" value={this.state.confirmPassword} onChange={this.handleConfirmPasswordChange} />
+          {!this.state.matchPassword && <label>Password doesn't match!</label>}
+        </div>
+        <div>
+          <label>Email:</label>
+          <input type="email" value={this.state.email} onChange={this.handleEmailChange} />
+        </div>
+        <Button type='button' onClick={this.handleSubmit}>Log In</Button>
+      </div>
+    )
+  }
+  
+
+  handleNameChange = (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  handleConfirmPasswordChange = (e) => {
+    this.setState({
+      confirmPassword: e.target.value
+    })
+  }
+
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  handleSubmit = () => {
+    const { 
+      name,
+      password,
+      email,
+      matchPassword,
+      uniqueName,
+      loadedUsers
+    } = this.state
+    this.checkMatch()
+    this.checkName()
+    if (matchPassword && loadedUsers && uniqueName) {
+      createUser(name, password, email)
+    }
+  }
+
+  checkMatch = () => {
+    const { password, confirmPassword } = this.state
+    const matchPassword = password == confirmPassword
+    this.setState({ matchPassword })
+  }
+
+  checkName = () => {
+    const uniqueName = !this.state.users.includes(this.state.name)
+    this.setState({ uniqueName })
+  }
+
+  componentDidMount = () => {
+    getUsers().then(users => {
+      const loadedUsers = true
+      this.setState({ users, loadedUsers })
+    })
+  }
+}
+
+export default Login
--- src/utils/connection.js
+++ src/utils/connection.js
@@ -2,9 +2,21 @@ import { get } from 'axios'
 import { URL } from '../config/config'
 
 const getToken = (username, password) => {
-  get(`${URL}users`, { params: { username, password }})
+  get(`${URL}users`, { params: { username, password } })
     .then(response => console.log(response))
     .catch(error => console.log(error))
 }
 
-export { getToken }
+const getUsers = () => {
+  get(`${URL}users`)
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+}
+
+const createUser = (name, password, email) => {
+  post(`${URL}users`, { params: {name, password, email } })
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+}
+
+export { getToken, getUsers, createUser }
