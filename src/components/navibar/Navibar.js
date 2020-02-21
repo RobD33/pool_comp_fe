293e9e8e78b10648781f8f610974699435c2b88d
Rob Davidson <rob@Robs-MacBook-Pro.local>
2020-02-21 13:44:08 +0000

    make navbar stateful

--- src/components/navibar/Navibar.js
+++ src/components/navibar/Navibar.js
@@ -2,87 +2,94 @@ import React from 'react'
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
 import './navibar.css'
 
-function Navibar({loggedIn, user, logOut, history, groupsForUser, selectGroup }) {
+class Navibar extends React.Component {
 
-  const navigateTo = (path, e) => {
+  state = {
+    expanded: false
+  }
+  
+  render() {
+    const { loggedIn, user, groupsForUser } = this.props
+    return (
+      <Navbar collapseOnSelect bg='dark' variant="dark" expand="lg" className='navbar'>
+        <Navbar.Brand href="home" >On The Baize</Navbar.Brand>
+        {loggedIn && <Navbar.Brand>{user.username}</Navbar.Brand>}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link
+              href="profile"
+              onClick={(e) => this.navigateTo('profile', e)}
+              disabled={!loggedIn}
+            >Profile</Nav.Link>
+            <NavDropdown
+              title="Groups"
+              id="nav-dropdown"
+              disabled={!loggedIn}
+            >
+              {groupsForUser.length && groupsForUser.map((group, index) => {
+                return (
+                  <NavDropdown.Item
+                    key={index}
+                    onClick={(e) => this.selectGroupAndNavigate(group, e) }
+                  >{group.name}</NavDropdown.Item>
+                )
+              })}
+              <NavDropdown.Item
+                onClick={ (e) => this.navigateTo('join-a-group', e) }
+              >Join a group</NavDropdown.Item>
+              <NavDropdown.Item
+                onClick={ (e) => this.navigateTo('create-a-group', e) }
+              >Create a group</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link
+              href="competitions"
+              onClick={(e) => this.navigateTo('competitions', e)}
+              disabled={!loggedIn}
+            >Competitions</Nav.Link>
+            <Nav.Link
+              href="stats"
+              onClick={(e) => this.navigateTo('stats', e)}
+              disabled={!loggedIn}
+            >Stats</Nav.Link>
+            <Nav.Link
+              href='login'
+              onClick={(e) => this.navigateTo('login', e)}
+              disabled={loggedIn}
+              active={!loggedIn}
+            >Log In</Nav.Link>
+            <Nav.Link
+              href='signup'
+              onClick={(e) => this.navigateTo('signup', e)}
+              disabled={loggedIn}
+              active={!loggedIn}
+            >Sign Up</Nav.Link>
+            <Nav.Link
+              href='home'
+              onClick={(e) => this.logOutAndNavigateTo('home', e)}
+              disabled={!loggedIn}
+              active={false}
+            >Log Out</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+  }
+
+  navigateTo = (path, e) => {
     e.preventDefault()
-    history.push(`/${path}`)
+    this.props.history.push(`/${path}`)
   }
 
-  const logOutAndNavigateTo = (path, e) => {
-    logOut()
-    navigateTo(path, e)
+  logOutAndNavigateTo = (path, e) => {
+    this.props.logOut()
+    this.navigateTo(path, e)
   }
 
-  const selectGroupAndNavigate = (group, e) => {
-    selectGroup(group)
-    navigateTo('groups', e)
+  selectGroupAndNavigate = (group, e) => {
+    this.props.selectGroup(group)
+    this.navigateTo('groups', e)
   }
-
-  return (
-    <Navbar collapseOnSelect bg='dark' variant="dark" expand="lg" className='navbar'>
-      <Navbar.Brand href="home" >On The Baize</Navbar.Brand>
-      {loggedIn && <Navbar.Brand>{user.username}</Navbar.Brand>}
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link
-            href="profile"
-            onClick={(e) => navigateTo('profile', e)}
-            disabled={!loggedIn}
-          >Profile</Nav.Link>
-          <NavDropdown
-            title="Groups"
-            id="nav-dropdown"
-            disabled={!loggedIn}
-          >
-            {groupsForUser.length && groupsForUser.map((group, index) => {
-              return (
-                <NavDropdown.Item
-                  key={index}
-                  onClick={(e) => selectGroupAndNavigate(group, e) }
-                >{group.name}</NavDropdown.Item>
-              )
-            })}
-            <NavDropdown.Item
-              onClick={ (e) => navigateTo('join-a-group', e) }
-            >Join a group</NavDropdown.Item>
-            <NavDropdown.Item
-              onClick={ (e) => navigateTo('create-a-group', e) }
-            >Create a group</NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link
-            href="competitions"
-            onClick={(e) => navigateTo('competitions', e)}
-            disabled={!loggedIn}
-          >Competitions</Nav.Link>
-          <Nav.Link
-            href="stats"
-            onClick={(e) => navigateTo('stats', e)}
-            disabled={!loggedIn}
-          >Stats</Nav.Link>
-          <Nav.Link
-            href='login'
-            onClick={(e) => navigateTo('login', e)}
-            disabled={loggedIn}
-            active={!loggedIn}
-          >Log In</Nav.Link>
-          <Nav.Link
-            href='signup'
-            onClick={(e) => navigateTo('signup', e)}
-            disabled={loggedIn}
-            active={!loggedIn}
-          >Sign Up</Nav.Link>
-          <Nav.Link
-            href='home'
-            onClick={(e) => logOutAndNavigateTo('home', e)}
-            disabled={!loggedIn}
-            active={false}
-          >Log Out</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  )
 }
 
 export default Navibar
